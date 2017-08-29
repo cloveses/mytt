d7f66a1e6d530ffe26c160759a0e07460fa2c34e
djx <djx@debiandjx.dj>
2017-08-29 22:35:39 +0800

    js

--- manage.py
+++ manage.py
@@ -12,5 +12,11 @@ async def hh(request):
     # return text(str(request.url))
     return html(env.get_template('index.html').render(var='World'))
 
+@app.route('/hc')
+async def hc(request):
+    key = request.args.get('key')
+    print(key)
+    return text(str(key))
+
 if __name__ == '__main__':
-    app.run(port=8000,debug=True)
+    app.run(host='192.168.1.105',port=8000,debug=True)
